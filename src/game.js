e7b7028116ab6cfdee6ac43c8772b6de581d65b6
James Pickering <james_pic@hotmail.com>
2013-11-22 17:28:49 +0000

    Bug-fix to crafty

--- src/game.js
+++ src/game.js
@@ -6,8 +6,8 @@ Game = {
     // Start crafty and set a background color so that we can see it's working
     Crafty.init(1100, 650);
     Crafty.background('grey');
+    Crafty.viewport.clampToEntities = false
     Crafty.scene("Load")
-    window.onresize = function() {Crafty.viewport.reload()}
     
   }
 }
